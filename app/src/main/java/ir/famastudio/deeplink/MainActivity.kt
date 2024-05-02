1c0e00a7627b6112d608320520477343e130d332
barbossa <barbossa.fama.dev@gmail.com>
2024-05-02 01:57:07 -0700

    first commit

---
+++ app/src/main/java/ir/famastudio/deeplink/MainActivity.kt
@@ -0,0 +1,26 @@
+package ir.famastudio.deeplink
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        if(intent.action == Intent.ACTION_VIEW){
+            val name = intent.data?.getQueryParameter("name")
+            findViewById<TextView>(R.id.textData).text = name
+        }
+    }
+}
